19181a1fbf4a15e8afcc439622d7feb5a2dd8c7a
chane81 <chane81@naver.com>
2018-09-22 10:53:23 +0900

    LESSON1 컴포넌트 추가

---
+++ lesson1/vue-intermediate-clone/vue-todo/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+   <TodoHeader></TodoHeader>
+   <TodoInput></TodoInput>
+   <TodoFooter></TodoFooter>
+   <TodoList></TodoList>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue'
+import TodoInput from './components/TodoInput.vue'
+import TodoFooter from './components/TodoFooter.vue'
+import TodoList from './components/TodoList.vue'
+
+// var my_cmp = {
+//   template: '<div>my component</div>'
+// }
+
+// new Vue({
+//   el:'',
+//   components: {
+//     'my-cmp': my_cmp
+//   }
+// });
+
+export default {
+  components: {
+    'TodoHeader': TodoHeader,
+    'TodoInput': TodoInput,
+    'TodoFooter': TodoFooter,
+    'TodoList': TodoList
+  }
+}
+</script>
+
+<style>
+</style>
