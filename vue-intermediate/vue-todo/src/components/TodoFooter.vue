3f296164b9079feb6c7ebfec939c371ad220e376
chane81 <chane81@naver.com>
2019-03-07 07:12:53 +0900

    코드 리팩토링 mapGetters, mapMutations 적용

--- vue-intermediate/vue-todo/src/components/TodoFooter.vue
+++ vue-intermediate/vue-todo/src/components/TodoFooter.vue
@@ -1,21 +1,20 @@
 <template>
   <div class="clearAllContainer">
-    <span
-      class="clearAllBtn"
-      v-on:click="clearTodo"
-    >
-      Clear All
-    </span>
+    <span class="clearAllBtn" v-on:click="clearTodo">Clear All</span>
   </div>
 </template>
 
 <script>
+import { mapMutations } from "vuex";
+
 export default {
   methods: {
-    clearTodo() {
-      //this.$emit("clearAll");
-      this.$store.commit("clearTodo");
-    }
+    // clearTodo() {
+    //   //this.$emit("clearAll");
+    //   this.$store.commit("clearTodo");
+    // }
+
+    ...mapMutations(["clearTodo"])
   }
 };
 </script>
