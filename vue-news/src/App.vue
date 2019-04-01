6412838569b185f9bbe8371548c379652f370630
BM <chane81@naver.com>
2019-04-02 04:08:26 +0900

    라우팅 링크에 스타일링 입히기

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <tool-bar></tool-bar>
-    <router-view></router-view>
+    <tool-bar />
+    <router-view />
   </div>
 </template>
 
@@ -16,12 +16,8 @@ export default {
 </script>
  
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  padding: 0;
+  margin: 0;
 }
 </style>
