9939b51991059c3ec1bb6e4195d6bafcbe108ad9
BM <chane81@naver.com>
2019-04-02 04:51:53 +0900

    News axios 불러오기

--- vue-news/README.md
+++ vue-news/README.md
@@ -22,3 +22,16 @@ npx @vue/cli create vue-news
     routes: []
   });
   ```
+
+## api url
+  - https://github.com/tastejs/hacker-news-pwas/blob/master/docs/api.md
+  - News
+    - https://api.hnpwa.com/v0/news/1.json
+  - Newest
+    - https://api.hnpwa.com/v0/newest/1.json
+  - Ask
+    - https://api.hnpwa.com/v0/ask/1.json
+  - Show
+    -	https://api.hnpwa.com/v0/show/1.json
+  - Jobs
+    - https://api.hnpwa.com/v0/jobs/1.json
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,12 +1,34 @@
 <template>
   <div>
-    News
+    <div
+      v-for="user in users"
+      :key="user.id"
+    >
+      {{ user.title }}
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+  data() {
+    return {
+      users: []
+    };
+  },
+  created() {
+    axios
+      .get('https://api.hnpwa.com/v0/news/1.json')
+      .then(res => {
+        console.log(res);
+        this.users = res.data;
+      })
+      .catch(err => {
+        console.log('err:', err);
+      });
+  }
 };
 </script>
 
