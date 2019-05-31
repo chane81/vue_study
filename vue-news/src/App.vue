47ddbb086fd0ae6f7e420981ce91e5854c941950
BM <chane81@naver.com>
2019-05-31 17:14:45 +0900

    News리스트 스타일링

--- vue-news/src/App.vue
+++ vue-news/src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="app">
-    <tool-bar />
+    <tool-bar/>
     <transition name="page">
-      <router-view />
+      <router-view/>
     </transition>
   </div>
 </template>
 
 <script>
-import ToolBar from './components/ToolBar';
+import ToolBar from "./components/ToolBar";
 
 export default {
   components: {
@@ -22,7 +22,17 @@ body {
   padding: 0;
   margin: 0;
 }
-
+a {
+  color: #34495e;
+  text-decoration: none;
+}
+a:hover {
+  color: #42b883;
+  text-decoration: underline;
+}
+a.router-link-exact-active {
+  text-decoration: underline;
+}
 .page-enter-active,
 .page-leave-active {
   transition: opacity 0.5s;
--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <p v-for="data in GET_NEWS" :key="data.id">
+    <!-- <p v-for="data in GET_NEWS" :key="data.id">
       <a v-bind:href="data.url">
         {{ data.title }}
       </a>
@@ -10,19 +10,40 @@
           {{ data.user }}
         </router-link>
       </small>
-    </p>
+    </p>-->
+    <ul class="news-list">
+      <li v-for="data in GET_NEWS" :key="data.id" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">{{ data.points }}</div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="news-title">
+            <a v-bind:href="data.url">{{ data.title }}</a>
+          </p>
+          <small class="link-text">
+            by
+            <router-link :to="`/user/${data.user}`" class="link-text">{{ data.user }}</router-link>
+          </small>
+        </div>
+        <!-- <a v-bind:href="data.url">{{ data.title }}</a>
+        <small>
+          {{ data.time_ago }} by
+          <router-link :to="`/user/${data.user}`">{{ data.user }}</router-link>
+        </small>-->
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters, mapActions } from "vuex";
 
 export default {
   computed: {
-    ...mapGetters(['GET_NEWS'])
+    ...mapGetters(["GET_NEWS"])
   },
   methods: {
-    ...mapActions(['FETCH_NEWS'])
+    ...mapActions(["FETCH_NEWS"])
   },
   created() {
     this.FETCH_NEWS();
@@ -30,4 +51,29 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
+</style>
