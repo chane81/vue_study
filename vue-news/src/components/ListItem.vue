aa5d63ddc430180639ff4e0bba35349e08494782
BM <chane81@naver.com>
2019-06-27 00:56:37 +0900

    news, jobs, ask 공통컴포넌트 추가

---
+++ vue-news/src/components/ListItem.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <ul class="list">
+      <li v-for="data in GET_DATA" :key="data.id" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">{{ data.points || 0 }}</div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="title">
+            <a v-bind:href="data.url">{{ data.title }}</a>
+          </p>
+          <small class="link-text">
+            <div v-if="routeName !== 'jobs'" name="by">
+              by
+              <router-link :to="`/user/${data.user}`" class="link-text">{{ data.user }}</router-link>
+            </div>
+            <div v-if="routeName === 'jobs'" name="by-jobs">
+              {{ data.time_ago }} by
+              <a :href="data.url">{{ data.domain }}</a>
+            </div>
+          </small>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  data() {
+    return {
+      routeName: this.$route.name
+    };
+  },
+  computed: {
+    //...mapGetters(["GET_DATA"])(this.routeName)
+    GET_DATA() {
+      return this.$store.getters.GET_DATA(this.routeName);
+    }
+  },
+  methods: {
+    ...mapActions(["FETCH_DATA"])
+  },
+  created() {
+    this.FETCH_DATA(this.routeName);
+  }
+};
+</script>
+
+<style scoped>
+.list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+.title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
+</style>
